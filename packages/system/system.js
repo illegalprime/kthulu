543c918ccf9510e1a65606b7807518dc695ed339
Michael Eden <themichaeleden@gmail.com>
2016-01-01 14:50:03 -0500

    added backed for volume control

--- packages/system/system.js
+++ packages/system/system.js
@@ -1,9 +1,18 @@
-Meteor.methods({
-    volume: function(value) {
-        if (!value) {
-            // TODO: Get volume
-            return 80;
+(function() {
+    var exec = Meteor.wrapAsync(Npm.require("child_process").exec);
+
+    Meteor.methods({
+        volume: function(value) {
+            if (!value) {
+                try {
+                    var output = exec("amixer -D pulse sget Master");
+                    return parseInt(output.match(/\[(\d+)%\]/)[1]);
+                } catch (err) {
+                    // TODO: Something better
+                    return 0;
+                }
+            }
+            exec("amixer -D pulse --quiet sset Master " + value + "%");
         }
-        console.log("Changing volume to " + value);
-    }
-});
+    });
+})();
