df90fe213fd5fe28f406f69bfc8ff1e8407e96db
Michael Eden <themichaeleden@gmail.com>
2016-01-01 17:13:28 -0500

    set DISPLAY variable in running wmctrl

--- packages/wmctrl/wmctrl.js
+++ packages/wmctrl/wmctrl.js
@@ -1,28 +1,38 @@
-var exec = Meteor.wrapAsync(Npm.require("child_process").exec);
+(function() {
+    "use strict";
 
-var actions = {
-    focus: function(opts) {
-        if (!_.isObject(opts)) {
-            throw new Meteor.Error("MUST_SPECIFY_CONSTRAINTS");
-        }
-        if (_.isString(opts.title)) {
-            exec("wmctrl -a '" + opts.title + "'");
-        } else if (_.isString(opts.class)) {
-            exec("wmctrl -x -a '" + opts.class + "'");
-        } else {
-            throw new Meteor.Error("NO_SUCH_WIN_SPECIFIER");
-        }
-    },
-};
-
-Meteor.methods({
-    wmctrl: function(options) {
-        _.each(options, function(args, method) {
-            if (_.isFunction(actions[method])) {
-                actions[method](args);
-            } else {
-                throw new Meteor.Error("NO_SUCH_METHOD", method);
-            }
+    var exec = function(cmd) {
+        return Meteor.wrapAsync(Npm.require("child_process").exec)(cmd, {
+            env: {
+                DISPLAY: ":0",
+            },
         });
-    },
-});
+    };
+
+    var actions = {
+        focus: function(opts) {
+            if (!_.isObject(opts)) {
+                throw new Meteor.Error("MUST_SPECIFY_CONSTRAINTS");
+            }
+            if (_.isString(opts.title)) {
+                exec("wmctrl -a '" + opts.title + "'");
+            } else if (_.isString(opts.class)) {
+                exec("wmctrl -x -a '" + opts.class + "'");
+            } else {
+                throw new Meteor.Error("NO_SUCH_WIN_SPECIFIER");
+            }
+        },
+    };
+
+    Meteor.methods({
+        wmctrl: function(options) {
+            _.each(options, function(args, method) {
+                if (_.isFunction(actions[method])) {
+                    actions[method](args);
+                } else {
+                    throw new Meteor.Error("NO_SUCH_METHOD", method);
+                }
+            });
+        },
+    });
+})();
