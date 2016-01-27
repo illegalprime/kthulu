228fcaed65b1a9057f3b891f050b98daeda11be0
Michael <themichaeleden@gmail.com>
2016-01-27 01:59:13 -0500

    added some input verification and error handling to spotify package

--- packages/spotify/spotify.js
+++ packages/spotify/spotify.js
@@ -3,13 +3,24 @@
 
     var DEFAULT_DISPLAY = ":0";
     var DEFAULT_DBUS_ADDR = "unix:path=/run/dbus/system_bus_socket";
+    var SPOTIFY_ID = /[0-9a-zA-Z]{22}/;
 
     process.env.DISPLAY = process.env.DISPLAY || DEFAULT_DISPLAY;
     process.env.DBUS_SESSION_BUS_ADDRESS =
         process.env.DBUS_SESSION_BUS_ADDRESS || DEFAULT_DBUS_ADDR;
 
     var SpotifyApi = (function() {
-        var exec = Meteor.wrapAsync(Npm.require("child_process").exec);
+        var exec = (function() {
+            var npmExec = Meteor.wrapAsync(Npm.require("child_process").exec);
+            return function(cmd) {
+                try {
+                    var out = npmExec(cmd);
+                    Log.info("dbus-reply", out);
+                } catch (err) {
+                    throw new Meteor.Error("NO_SPOTIFY");
+                }
+            };
+        })();
         var self = this;
         var prefix = "dbus-send --print-reply " +
             "--dest=org.mpris.MediaPlayer2.spotify " +
@@ -25,12 +36,12 @@
         };
 
         self.play = function(id) {
-            // TODO add verification
+            if (!_.isString(id) || !id.match(SPOTIFY_ID)) {
+                throw new Meteor.Error("INVALID_SPOTIFY_ID");
+            }
             var cmd = prefix + methods.play + " " + id_to_dbus(id);
             Log.info("dbus call:", cmd);
-
-            var out = exec(cmd);
-            Log.info("dbus-reply", out);
+            exec(cmd);
         };
 
         return self;
